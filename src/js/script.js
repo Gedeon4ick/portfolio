90be98a717d7810e9a1d73d738c75b23cf07cb7c
Gedeon4ick <pavell720@gmaul.com>
2022-11-09 18:13:52 +0600

    gmail

--- src/js/script.js
+++ src/js/script.js
@@ -80,4 +80,22 @@ document.addEventListener('scroll', function () {
         });
 
     }
-})
+});
+
+$('form').submit(function(e) {
+    e.preventDefault();
+    
+    // if(!$(this).valid()) {
+    //     return;
+    // }
+    
+    $.ajax({
+        type: "POST",
+        url: "mailer/smart.php",
+        data: $(this).serialize()
+    }).done(function() {
+        $(this).find("input").val("");
+        $('form').trigger('reset');
+    });
+    return false;
+});
