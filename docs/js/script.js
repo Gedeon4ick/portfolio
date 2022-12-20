54fcc0808aaecc78493c72b01134b0b84b5b4917
Gedeon4ick <pavell720@gmaul.com>
2022-12-20 13:42:00 +0600

    slider js

--- docs/js/script.js
+++ docs/js/script.js
@@ -39,7 +39,7 @@ const anchors = document.querySelectorAll('a[href*="#"]');
 anchors.forEach(anchor => {
     anchor.addEventListener('click', (e) => {
         e.preventDefault();
-        console.log(anchor.href)
+        // console.log(anchor.href)
 
         const blockID = anchor.getAttribute('href').substr(1)
         document.getElementById(blockID).scrollIntoView({
@@ -58,14 +58,14 @@ document.addEventListener('scroll', function () {
     if (pageYOffset > promo.offsetHeight) {
         hamSpan.forEach(element => {
             element.style.background = 'black';
-            console.log(sidepanel.childNodes);
-            console.log(sidepanel.childNodes[0].childNodes[0].childNodes);
+            // console.log(sidepanel.childNodes);
+            // console.log(sidepanel.childNodes[0].childNodes[0].childNodes);
 
             sidepanel.childNodes[0].childNodes[0].childNodes[0].setAttribute("fill", "#000");
             sidepanel.childNodes[1].childNodes[0].childNodes[0].setAttribute("fill", "#000");
             sidepanel.childNodes[2].childNodes[0].childNodes[0].setAttribute("fill", "#000");
             sidepanel.childNodes[4].style.color = "black";
-            sidepanel.childNodes[3].style.backgroundColor =  "black";
+            sidepanel.childNodes[3].style.backgroundColor = "black";
         });
     } else {
         hamSpan.forEach(element => {
@@ -73,8 +73,8 @@ document.addEventListener('scroll', function () {
             sidepanel.childNodes[0].childNodes[0].childNodes[0].setAttribute("fill", "white");
             sidepanel.childNodes[1].childNodes[0].childNodes[0].setAttribute("fill", "white");
             sidepanel.childNodes[2].childNodes[0].childNodes[0].setAttribute("fill", "white");
-            sidepanel.childNodes[3].style.backgroundColor =  "white";
-            sidepanel.childNodes[4].style.color =  "white";
+            sidepanel.childNodes[3].style.backgroundColor = "white";
+            sidepanel.childNodes[4].style.color = "white";
 
 
         });
@@ -82,20 +82,117 @@ document.addEventListener('scroll', function () {
     }
 });
 
-$('form').submit(function(e) {
+$('form').submit(function (e) {
     e.preventDefault();
-    
+
     // if(!$(this).valid()) {
     //     return;
     // }
-    
+
     $.ajax({
         type: "POST",
         url: "mailer/smart.php",
         data: $(this).serialize()
-    }).done(function() {
+    }).done(function () {
         $(this).find("input").val("");
         $('form').trigger('reset');
     });
     return false;
+});
+
+// slider
+// родительский блок всего
+const slider = document.querySelector('.offer');
+// сами сладеры
+const slides = document.querySelectorAll('.wrapperSl__slides_slide');
+// главная обертка
+const slidesWrapper = document.querySelector('.wrapperSl');
+// поле с нашими слайдами
+const slidesField = document.querySelector('.wrapperSl__slides');
+const prev = document.querySelector('.offer__counter_prev');
+const next = document.querySelector('.offer__counter_next');
+// получение ширины окна через которые мы смотрим на слайды
+const width = window.getComputedStyle(slidesWrapper).width;
+
+// слайд индекс будет определять какой слайд
+let slideIndex = 1;
+// переменная которая будет говорить нам на сколько мы отступили в право и лево
+let offset = 0;
+
+slidesField.style.width = 100 * slides.length + '%';
+slides.forEach(slide => {
+    slide.style.width = width;
+});
+
+
+
+const indicators = document.createElement('ol');
+
+const dots = [];
+indicators.classList.add('carousel-indicators');
+
+slider.append(indicators);
+
+for (let i = 0; i < slides.length; i++) {
+    const dot = document.createElement('li');
+    dot.setAttribute('data-slide-to', i + 1);
+    // активное состояние dots
+    if (i == 0) {
+        dot.style.opacity = 1;
+    }
+    indicators.append(dot);
+    dots.push(dot);
+}
+next.addEventListener('click', () => {
+    if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) {
+        offset = 0;
+    } else {
+        offset += +width.slice(0, width.length - 2);
+    }
+
+    if (slideIndex == slides.length) {
+        // если долистали до 4 то переносим индекс до значения 1
+        slideIndex = 1;
+    } else {
+        slideIndex++;
+    }
+
+    slidesField.style.transform = `translateX(-${offset}px)`;
+
+    dots.forEach(dot => dot.style.opacity = '.5');
+    dots[slideIndex - 1].style.opacity = 1;
+});
+
+prev.addEventListener('click', () => {
+    if (offset == 0) {
+        offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+    } else {
+        offset -= +width.slice(0, width.length - 2);
+    }
+
+    if (slideIndex == 1) {
+        // если долистали до 4 то переносим индекс до значения 1
+        slideIndex = slides.length;
+    } else {
+        slideIndex--;
+    }
+
+    slidesField.style.transform = `translateX(-${offset}px)`;
+
+    dots.forEach(dot => dot.style.opacity = '.5');
+    dots[slideIndex - 1].style.opacity = 1;
+});
+
+dots.forEach(dot => {
+    dot.addEventListener('click', (e) => {
+        const slideTo = e.target.getAttribute('data-slide-to');
+        slideIndex = slideTo;
+        offset = +width.slice(0, width.length - 2) * (slideTo - 1);
+
+        slidesField.style.transform = `translateX(-${offset}px)`;
+
+        dots.forEach(dot => dot.style.opacity = '.5');
+        dots[slideIndex - 1].style.opacity = 1;
+
+    });
 });
